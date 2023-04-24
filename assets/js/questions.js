3fd19b713dc0ae444280ee36b9c8c1c3ecea3685
BlanchardConnor <BlanchardConnor73@gmail.com>
2023-04-24 17:46:38 -0500

    Questions displayded through JS, commands given to CSS through JS. bug on question 4. math, timer, and highscores not yet completed

---
+++ assets/js/questions.js
@@ -0,0 +1,66 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does CSS stand for?",
+        answer: "Cascading style sheets",
+        options: [
+            "Corrosive spinal stimulants",
+            "Cascading scale sheets",
+            "Cascading style sheets",
+            "Why are you asking me? You're the big 'Mr. Coder'"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "How do you create a directory in Bash?",
+        answer: "mkdir code_quiz",
+        options: [
+        "mkdir code_quiz",
+        "makedir code_quiz",
+        "md code_quiz",
+        "git mkdir code_quiz"
+    ]
+},
+
+{
+    numb: 3,
+    question: "What is the most elusive coding bug?",
+    answer: "Missing semicolons",
+    options: [
+        "Missing quotation marks",
+        "Cockroaches",
+        "Missing periods",
+        "Missing semicolons"
+    ]
+},
+
+{
+    numb: 4,
+    question: "What does HTML stand for?",
+    answer: "Hypertext Markup Language",
+    options: [
+        "Hovering Text Market Lights",
+        "Hickory-smoked Thick Meat Legs",
+        "Hyper Text Markup Language",
+        "Hyper Text Markup Logging"
+    ]
+},
+
+{
+    numb: 5,
+    question: "Why are software engineers always single?",
+    answer: "Yes",
+    options: [
+        "Committing issues",
+        "Socially inept",
+        "Var loveLife is called, but never determined",
+        "Yes"
+    ]
+},
+ ];
+
+ 
+ 
+
+ 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
+// collecting required HTML elements
 const highScores = document.querySelector(".high-scores");
 const dropdown = document.querySelector(".dropdown-btn");
 const scores = document.querySelector(".scores");
 const startBtn = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
-const quitBtn = document.querySelector(".buttons .quit");
-const retryBtn = document.querySelector(".buttons .retry");
-const questions = document.querySelector(".questions");
+const quitBtn = infoBox.querySelector(".buttons .quit");
+const continueBtn = document.querySelector(".buttons .retry");
+const quizBox = document.querySelector(".quiz-box")
 const finishScreen = document.querySelector(".finish-screen");
-const answers = document.querySelector(".answers");
 const timeText = document.querySelector(".timer .timer-txt");
 const timeCount = document.querySelector(".timer .timer-sec");
+const bottomQueCounter = document.querySelector("footer .total-que");
+const answers = document.querySelector(".answers");
 
 //Shows and hides dropdown menu with highscores //
 function showScores() {
@@ -24,32 +26,84 @@ dropdown.addEventListener("click", showScores);
 
 
 // Shows info screen if start button is clicked //
- function showInfo() {
-     if (infoBox.style.display === "none") {
-         infoBox.style.display = "block";
-     }
-  else {
-     infoBox.style.display = "none";
+ startBtn.onclick = ()=> {
+    infoBox.classList.add("activeInfo");
  }
- }
- startBtn.addEventListener("click", showInfo);
 
 
  // Hides info screen if quit button is clicked //
- function hideInfo() {
-    if (infoBox.style.display === "block") {
-        infoBox.style.display = "none";
-    }
+ quitBtn.onclick = ()=> {
+infoBox.classList.remove("activeInfo");
  }
- quitBtn.addEventListener("click", hideInfo);
 
  // if continue button is clicked //
- function continueBtn() {
-    if (questions.style.display === "none") {
-        questions.style.display = "block";
+ continueBtn.onclick = ()=> {
+    infoBox.classList.remove("activeInfo"); // hides info screen //
+    quizBox.classList.add("activeQuiz"); // shows quiz box //
+    showQuestions(0);
+    queCounter(1);
+     }
+
+let questionCnt = 0;
+let questionNumb = 1;
+
+const nextBtn = quizBox.querySelector("footer .next-btn");
+// if Next Question button is clicked //
+nextBtn.onclick =()=> {
+    if (questionCnt < questions.length - 1) {
+        questionCnt++;
+        questionNumb++;
+        showQuestions(questionCnt);
+        queCounter(questionNumb);
     }
     else {
-        questions.style.display = "none";
+        console.log("questions completed");
     }
- }
- retryBtn.addEventListener("click", continueBtn);
+}
+
+// getting questions and corresponding answers from questions.js array //
+function showQuestions(index) {
+    const questionTxt = document.querySelector(".question-txt");
+    let questionTag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let answersTag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+                        + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+                        + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+                        + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    questionTxt.innerHTML = questionTag;
+    answers.innerHTML = answersTag;
+    const option = answers.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer) {
+    let userAns = answer.textContent;
+    let correctAns = questions[questionCnt].answer;
+    let allAns = answers.children.length;
+    if (userAns == correctAns) {
+        console.log("Correct answer!");
+        answer.classList.add("correct");
+    }
+    else {
+        console.log(userAns , "is incorrect,");
+        console.log("Correct answer is:" , correctAns);
+        answer.classList.add("incorrect");
+    }
+
+    for (let i = 0; i < allAns; i++) {
+        answers.children[i].classList.add("disabled");
+    }
+
+    for (let i = 0; i < allAns; i++) {
+        if (answers.children[i].textContent == correctAns) {
+            answers.children[i].setAttribute("class", "option correct");
+        }
+    }
+}
+
+function queCounter(index) {
+const bottomQueCounter = quizBox.querySelector("footer .total-que");
+let totalQueCntTag = '<span><p>'+ index +'</p> <p>Of</p> <p>'+ questions.length +'</p> <p>Questions</p></span>';
+bottomQueCounter.innerHTML = totalQueCntTag; 
+}
